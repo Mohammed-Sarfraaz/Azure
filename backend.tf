9d332d6dd87869c2cc7c23c677620eb96ca04acd
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-01 15:51:09 +0000

    updated

--- backend.tf
+++ backend.tf
@@ -8,4 +8,5 @@ terraform {
     tenant_id            = "35b74ca5-c625-4346-9761-e208d0467290"
     subscription_id      = "7809a880-b293-43cb-91e4-8f08b04e1c09"
     client_id            = "500d4c14-bc2e-4ca3-b814-05b8149997e0"
+  }
 }
