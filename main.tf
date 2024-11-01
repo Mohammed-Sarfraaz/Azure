7510bc643f741b3e41f1567fc7ad97f4306ca0b1
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-01 16:46:22 +0000

    updated

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_api_management" "apim" {
-  name                = "mssapim"
+  name                = "mssapim2"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   publisher_name      = "Sarfraaz"
