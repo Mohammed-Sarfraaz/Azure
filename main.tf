a89dd4f599d4d8fcf80d5fcc02f843025f9927ee
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-01 01:47:07 +0000

    updated

--- main.tf
+++ main.tf
@@ -47,21 +47,6 @@ resource "azurerm_api_management" "apim" {
   sku_name            = "Developer_1"
 }
 
-resource "azurerm_api_management_api" "sampleapi" {
-  name                = "PetstoreAPI"
-  resource_group_name = azurerm_resource_group.rg.name
-  api_management_name = azurerm_api_management.apim.name
-  revision            = "1"
-  display_name        = "Petstore API"
-  path                = "PetsAPI"
-  protocols           = ["https"]
-
-  import {
-    content_format = "swagger-link-json"
-    content_value  = "https://github.com/OAI/OpenAPI-Specification/blob/main/examples/v3.0/petstore.json"
-  }
-}
-
 output "api_management_url" {
   value = azurerm_api_management.apim.gateway_url
 }
