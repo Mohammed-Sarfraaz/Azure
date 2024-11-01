7e5f52a108a6ffdb7e949efd08dd5086987a9d4c
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-01 15:33:38 +0000

    updated

--- backend.tf
+++ backend.tf
@@ -4,5 +4,9 @@ terraform {
     storage_account_name = "stgtfstate"
     container_name       = "contfstate"
     key                  = "apim.tfstate"
+    use_oidc             = true
+    tenant_id            = env.ARM_TENANT_ID
+    subscription_id      = env.ARM_SUBSCRIPTION_ID
+    client_id            = env.ARM_CLIENT_ID
   }
 }
---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+        type = string
+        sensitive = true
+}
+
+variable "tenant_id" {
+        type = string
+        sensitive = true
+}
+
+variable "client_id" {
+        type = string
+        sensitive = true
+}
