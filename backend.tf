8b10e82cfb34f1290300b26f1899ac891f86a128
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-02 01:13:46 +0000

    updated

--- backend.tf
+++ backend.tf
@@ -5,8 +5,8 @@ terraform {
     container_name       = "contfstate"
     key                  = "apim.tfstate"
     use_oidc             = true
-    tenant_id            = "35b74ca5-c625-4346-9761-e208d0467290"
-    subscription_id      = "7809a880-b293-43cb-91e4-8f08b04e1c09"
-    client_id            = "500d4c14-bc2e-4ca3-b814-05b8149997e0"
+    tenant_id            = var.tenant_id
+    subscription_id      = var.subscription_id
+    client_id            = var.client_id
   }
 }
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type      = string
+  sensitive = true
+}
