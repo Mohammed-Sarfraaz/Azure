6273c039a539c13b223d8a0afa7fb744ed32b983
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-04 21:47:08 +0000

    added first api

--- main.tf
+++ main.tf
@@ -11,3 +11,53 @@ resource "azurerm_api_management" "apim" {
   publisher_email     = "sarfraaz-nov-24@outlook.com"
   sku_name            = "Developer_1"
 }
+
+
+
+# create API
+resource "azurerm_api_management_api" "apiHealthProbe" {
+name                = "health-probe"
+resource_group_name = azurerm_resource_group.rg.name
+api_management_name = azurerm_api_management.apim.name
+revision            = "1"
+display_name        = "Health probe"
+path                = "health-probe"
+protocols           = ["https"]
+
+  subscription_key_parameter_names  {
+    header = "AppKey"
+    query = "AppKey"
+  }
+
+  import {
+    content_format = "swagger-json"
+    content_value  = <<JSON
+      {
+          "swagger": "2.0",
+          "info": {
+              "version": "1.0.0",
+              "title": "Health probe"
+          },
+          "host": "not-used-direct-response",
+          "basePath": "/",
+          "schemes": [
+              "https"
+          ],
+          "consumes": [
+              "application/json"
+          ],
+          "produces": [
+              "application/json"
+          ],
+          "paths": {
+              "/": {
+                  "get": {
+                      "operationId": "get-ping",
+                      "responses": {}
+                  }
+              }
+          }
+      }
+    JSON
+  }
+}
