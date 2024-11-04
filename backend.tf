2cb0f28ea2b825774d45e392bf54778ae36520df
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-04 19:37:23 +0000

    updated

--- backend.tf
+++ backend.tf
@@ -3,6 +3,9 @@ terraform {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "stgtfstate"
     container_name       = "contfstate"
-    key                  = "apim.tfstate" 
+    key                  = "apim.tfstate"
+    use_oidc             = true
+    tenant_id            = "5b74ca5-c625-4346-9761-e208d0467290"
+    subscription_id      = "7809a880-b293-43cb-91e4-8f08b04e1c09"
   }
 }
