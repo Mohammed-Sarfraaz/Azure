fdba0e871275bf19b9bd8400017c2474332f51af
Sarfraaz Syed <sarfraaz@outlook.com>
2024-11-08 02:05:17 +0000

    updated

--- scripts/init-setup.sh
+++ scripts/init-setup.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-. ./init-env-vars.sh
+export LOCATION=useast
+export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg-786
+export TF_STATE_STORAGE_ACCOUNT_NAME=tfstatestracct-786
+export TF_STATE_CONTAINER_NAME=tfstate-containe-786
+export KEYVAULT_NAME=terraform-ref-kv-786
 
 # Create the resource group
 if  ( `az group exists --resource-group $COMMON_RESOURCE_GROUP_NAME` == "true" );
--- scripts/init-vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export LOCATION=useast
-export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg-786
-export TF_STATE_STORAGE_ACCOUNT_NAME=tfstatestracct-786
-export TF_STATE_CONTAINER_NAME=tfstate-containe-786
-export KEYVAULT_NAME=terraform-ref-kv-786
